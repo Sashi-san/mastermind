0ace1acbb5784a6b4d4c0daf83deb6dbfaa7ccc0
Sashi <otaku.nyaa@gmail.com>
2019-05-30 01:05:53 +0200

    ajout des fonctions nécessaires intToString et stringToInt

--- src/GameMode.java
+++ src/GameMode.java
@@ -10,17 +10,23 @@ public class GameMode {
     private static int secretNumber;
     private static int userNumber;
     private static String userStringNumber;
+    private static int life = 5;
+    private static boolean result = false;
 
     public static void challenger() {
         generateNumber();
-        showSecretNumber();
-        playGame(getUserNumber());
+        while (life > 0 || !result) {
+            showSecretNumber();
+            playGame(getUserNumber());
+        }
         gameOver();
     }
 
     private static void generateNumber() {
         Random random = new Random();
-        secretNumber=5223;
+        random.nextInt();
+        secretNumber = 5223;
+        //todo faire en sorte que cela est aléatoire dans un tableau.
         /*secretNumber = random.nextInt();
         if (secretNumber<0){
             secretNumber=secretNumber*-1;
@@ -28,21 +34,43 @@ public class GameMode {
         secretNumber=secretNumber%10000;*/
     }
 
-    private static int getUserNumber() {
-        Scanner clavier = new Scanner(System.in);
-        userStringNumber=clavier.nextLine();
-        return 0;
-    }
-
     private static void showSecretNumber() {
         System.out.println("Combinaison secrète :" + secretNumber);
     }
 
+    private static int getUserNumber() {
+        Scanner clavier = new Scanner(System.in);
+        userStringNumber = clavier.nextLine();
+        userNumber = Integer.valueOf(userStringNumber);
+        System.out.println();
+        return userNumber;
+    }
+
     private static void playGame(int userNumber) {
+     int secretNumberTab[]={0,0,0,0};
+     int userNumberTab[]={0,0,0,0};
+
 
     }
 
     private static void gameOver() {
+        if (life == 0) {
+            System.out.println("Dommage, le nombre secret était le 5223");
+        } else if (result) {
+            System.out.println("Bravo! Vous avez trouvé le nombre secret!");
+        }
+        System.out.println("Voulez vous recommencer ou quitter?");
+        System.out.println("1 - Recommencer");
+        System.out.println("2 - Quitter");
+    }
 
+    private static String intToString(int userNumber) {
+        userStringNumber = String.valueOf(userNumber);
+        return userStringNumber;
+    }
+
+    private static int stringToInt(String userStringNumber) {
+        userNumber = Integer.valueOf(userStringNumber);
+        return userNumber;
     }
 }
